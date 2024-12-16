45b1fc5aee9e36e7b9897881485bcc9cd9f299a4
sivaganesz <ganeshzsiva85@gmail.com>
2024-12-16 18:14:30 +0530

    reprogram

--- frontend/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import {RouterProvider} from 'react-router-dom'
-import './index.css'
-import router from './Router/Router.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>,
-)
